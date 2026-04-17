4639616bac1336b432a1fdd90dfc8d740cf6f079
Suchikkon Naskar <suchikkon0@gmail.com>
2026-04-17 21:43:30 +0530

    changes in order.controller.js

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -7,54 +7,54 @@ const asyncHandler = require("../utils/asyncHandler");
 
 
 exports.createOrder = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
-    const { addressId, paymentMethod = "cod" } = req.body;
+  const userId = req.user._id;
+  const { addressId, paymentMethod = "cod" } = req.body;
 
-    const address = await Address.findOne({
-      _id: addressId,
-      user: userId,
-    });
+  const address = await Address.findOne({
+    _id: addressId,
+    user: userId,
+  });
 
-    if (!address) {
-      throw new ApiError(404, "Address not found")
-    }
+  if (!address) {
+    throw new ApiError(404, "Address not found")
+  }
 
-    const cart = await Cart.findOne({ user: userId }).populate("items.item");
+  const cart = await Cart.findOne({ user: userId }).populate("items.item");
 
-    if (!cart || cart.items.length === 0) {
-      throw new ApiError(400, "Cart is empty")
-    }
+  if (!cart || cart.items.length === 0) {
+    throw new ApiError(400, "Cart is empty")
+  }
 
-    const orderItems = cart.items.map((i) => ({
-      item: i.item._id,
-      name: i.item.name,
-      price: i.price,
-      quantity: i.quantity,
-      image: i.item.image?.url,
-    }));
+  const orderItems = cart.items.map((i) => ({
+    item: i.item._id,
+    name: i.item.name,
+    price: i.price,
+    quantity: i.quantity,
+    image: i.item.image?.url,
+  }));
 
-    const order = await Order.create({
-      user: userId,
-      items: orderItems,
-      totalItems: cart.totalItems,
-      totalPrice: cart.totalPrice,
-      paymentMethod,
+  const order = await Order.create({
+    user: userId,
+    items: orderItems,
+    totalItems: cart.totalItems,
+    totalPrice: cart.totalPrice,
+    paymentMethod,
 
-      shippingAddress: {
-        fullName: address.fullName,
-        phone: address.phone,
-        addressLine: address.addressLine,
-        city: address.city,
-        state: address.state,
-        postalCode: address.postalCode,
-        country: address.country,
-      },
-    })
+    shippingAddress: {
+      fullName: address.fullName,
+      phone: address.phone,
+      addressLine: address.addressLine,
+      city: address.city,
+      state: address.state,
+      postalCode: address.postalCode,
+      country: address.country,
+    },
+  })
 
-    cart.items = [];
-    cart.totalItems = 0;
-    cart.totalPrice = 0;
-    await cart.save();
+  cart.items = [];
+  cart.totalItems = 0;
+  cart.totalPrice = 0;
+  await cart.save();
 
-    res.json(new ApiResponse(201, "Order plased Successfully", order))
+  res.json(new ApiResponse(201, "Order plased Successfully", order))
 });
